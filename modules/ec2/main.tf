0026e31a837bfa7565e973c0f38aa91f23a59154
cloudinstitution <134688786+cloudinstitution@users.noreply.github.com>
2025-04-30 20:57:33 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f1dcc636b69a6438"
+  ami           = "ami-0f1dcc636b69a6438"  # This Ami is for Mumbai region- "ap-south-1"Make sure to use the correct AMI for the specific region.
   instance_type = "t2.micro"
 
   tags = {
