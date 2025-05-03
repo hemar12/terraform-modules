b7f83e7f424df913d2a0e4984d435503d6bdd10b
cloudinstitution <134688786+cloudinstitution@users.noreply.github.com>
2025-05-03 09:11:21 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,26 +1,14 @@
 resource "aws_iam_user" "cloud" {
-    name = "cloud-institute-user"
-    tags = {
-        Name = "cloud-user"
-    }
+  name = "cloud-institute-user"
 }
-
-
 
 resource "aws_iam_user" "cloud-count" {
-    count = 5
-    name = "cloud-institute-user-${count.index + 1}"
-    tags = {
-        Name = "cloud-user-${count.index + 1}"
-    }
+  count = 5
+  name  = "cloud-institute-user-${count.index + 1}"
 }
-
 
 resource "aws_iam_user" "cloud-forset-user" {
-    for_each = toset(["sam", "pam", "kim", "tom"])
-    name = "cloud-institute-user-${each.value}"
-    tags = {
-        Name = "cloud-user-${each.value}"
-    }
+  for_each = toset(["sam", "pam", "kim", "tom"])
+  name     = each.value
 }
-  
+
