40e969e820c9c8a535ee2b2760d98a7c74061119
harris-97 <harrisnetto@gmail.com>
2025-05-07 17:48:33 +0530

    added azure and aws modules inside modules

--- modules.tf
+++ modules.tf
@@ -1,15 +1,15 @@
 module "iam" {
-  source = "./modules/iam/"
+  source = "./modules/aws/iam"
 }
 
 module "ec2" {
-  source = "./modules/ec2/"
+  source = "./modules/aws/ec2/"
 }
 
 module "vpc" {
-  source = "./modules/vpc/"
+  source = "./modules/aws/vpc/"
 }
 
 module "s3" {
-  source = "./modules/s3/"
+  source = "./modules/aws/s3/"
 }
